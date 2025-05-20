cafd9fed70553b2f1a08d71398286fa3c032a5df
Emilynqa309 <Emily.quindi27@myhunter.cuny.edu>
2025-05-20 10:16:12 -0400

    Add files via upload
    Named Midterm but is the Final based off Midterm

---
+++ midterm/main.js
@@ -0,0 +1,233 @@
+$(document).ready(function () {
+    $("#hoverImage1").hover(
+      function () {
+        // Mouse enters
+        $(this).attr("src", "images/selenavinyl.webp");
+      },
+      function () {
+        // Mouse leaves
+        $(this).attr("src", "images/selenar.webp");
+      }
+    );
+  });
+
+  $(document).ready(function () {
+    $("#hoverImage2").hover(
+      function () {
+        // Mouse enters
+        $(this).attr("src", "images/arianavinyl.webp");
+      },
+      function () {
+        // Mouse leaves
+        $(this).attr("src", "images/arianar.webp");
+      }
+    );
+  });
+
+  $(document).ready(function () {
+    $("#hoverImage3").hover(
+      function () {
+        // Mouse enters
+        $(this).attr("src", "images/chappellvinyl.webp");
+      },
+      function () {
+        // Mouse leaves
+        $(this).attr("src", "images/chappellr.webp");
+      }
+    );
+  });
+
+  $(document).ready(function () {
+    $("#hoverImage4").hover(
+      function () {
+        // Mouse enters
+        $(this).attr("src", "images/fayevinyl.webp");
+      },
+      function () {
+        // Mouse leaves
+        $(this).attr("src", "images/fayer.webp");
+      }
+    );
+  });
+
+  $(document).ready(function () {
+    $("#hoverImage5").hover(
+      function () {
+        // Mouse enters
+        $(this).attr("src", "images/billievinyl.jpeg");
+      },
+      function () {
+        // Mouse leaves
+        $(this).attr("src", "images/billier.webp");
+      }
+    );
+  });
+
+  $(document).ready(function () {
+    $("#hoverImage6").hover(
+      function () {
+        // Mouse enters
+        $(this).attr("src", "images/sabrinavinyl.webp");
+      },
+      function () {
+        // Mouse leaves
+        $(this).attr("src", "images/sabrinar.webp");
+      }
+    );
+  });
+
+  $(document).ready(function () {
+    $("#hoverImage7").hover(
+      function () {
+        // Mouse enters
+        $(this).attr("src", "images/taylorvinyl.webp");
+      },
+      function () {
+        // Mouse leaves
+        $(this).attr("src", "images/taylorr.webp");
+      }
+    );
+  });
+
+  $(document).ready(function () {
+    $("#hoverImage8").hover(
+      function () {
+        // Mouse enters
+        $(this).attr("src", "images/weekndvinyl.webp");
+      },
+      function () {
+        // Mouse leaves
+        $(this).attr("src", "images/weekndr.webp");
+      }
+    );
+  });
+
+  $(document).ready(function () {
+    $("#hoverImage9").hover(
+      function () {
+        // Mouse enters
+        $(this).attr("src", "images/mariasvinyl.webp");
+      },
+      function () {
+        // Mouse leaves
+        $(this).attr("src", "images/mariasr.webp");
+      }
+    );
+  });
+
+  $(document).ready(function () {
+    $("#hoverImage10").hover(
+      function () {
+        // Mouse enters
+        $(this).attr("src", "images/snrrvinyl.webp");
+      },
+      function () {
+        // Mouse leaves
+        $(this).attr("src", "images/snrr.webp");
+      }
+    );
+  });
+  
+  $(document).ready(function () {
+    $("#hoverImage11").hover(
+      function () {
+        // Mouse enters
+        $(this).attr("src", "images/ohmsvinyl.webp");
+      },
+      function () {
+        // Mouse leaves
+        $(this).attr("src", "images/ohms.webp");
+      }
+    );
+  });
+
+  $(document).ready(function () {
+    $("#hoverImage12").hover(
+      function () {
+        // Mouse enters
+        $(this).attr("src", "images/atfrvinyl.webp");
+      },
+      function () {
+        // Mouse leaves
+        $(this).attr("src", "images/atfr.webp");
+      }
+    );
+  });
+
+  $(document).ready(function () {
+    $("#hoverImage13").hover(
+      function () {
+        // Mouse enters
+        $(this).attr("src", "images/wbwurvinyl.webp");
+      },
+      function () {
+        // Mouse leaves
+        $(this).attr("src", "images/wbwur.webp");
+      }
+    );
+  });
+
+  $(document).ready(function () {
+    $("#hoverImage14").hover(
+      function () {
+        // Mouse enters
+        $(this).attr("src", "images/hitchvinyl.jpg");
+      },
+      function () {
+        // Mouse leaves
+        $(this).attr("src", "images/hitchr.webp");
+      }
+    );
+  });
+
+  $(document).ready(function () {
+    $("#hoverImage15").hover(
+      function () {
+        // Mouse enters
+        $(this).attr("src", "images/ridinvinyl.webp");
+      },
+      function () {
+        // Mouse leaves
+        $(this).attr("src", "images/ridin.webp");
+      }
+    );
+  });
+
+  $(document).ready(function () {
+    $("#hoverImage16").hover(
+      function () {
+        // Mouse enters
+        $(this).attr("src", "images/cwsrvinyl.webp");
+      },
+      function () {
+        // Mouse leaves
+        $(this).attr("src", "images/cwsr.webp");
+      }
+    );
+  });
+
+  $(document).ready(function () {
+    $("#hoverImage17").hover(
+      function () {
+        // Mouse enters
+        $(this).attr("src", "images/marvinyl.webp");
+      },
+      function () {
+        // Mouse leaves
+        $(this).attr("src", "images/mar.webp");
+      }
+    );
+  });
+
+  $(document).ready(function () {
+    $("#hoverImage18").hover(
+      function () {
+        // Mouse enters
+        $(this).attr("src", "images/jolvinyl.webp");
+      },
+      function () {
+        // Mouse leaves
+        $(this).attr("src", "images/jol.webp");
+      }
+    );
+  });
